9d5ca11b139d387bc04dcec4c40ae849cb94c56a
Onore849 <rion3376@gmail.com>
2020-09-10 16:36:39 +0900

    Initial Commit

---
+++ popUpMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  popUpMenu
+//
+//  Created by 野澤拓己 on 2020/09/10.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
